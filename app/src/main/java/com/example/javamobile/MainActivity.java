052f2f7be262dd4338ff60e2ef4b225dedca040b
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-11-05 10:51:48 +0530

    wevieww

--- app/src/main/java/com/example/javamobile/MainActivity.java
+++ app/src/main/java/com/example/javamobile/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.javamobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar mProgressBar;
+    private WebView mWebView;
+
+    private String urlToload = "https://www.google.lk/webhp?hl=en&sa=X&ved=0ahUKEwiZwtiMpsSJAxWEzjgGHavjD5QQPAgI";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //assiging view by id
+        mProgressBar = findViewById(R.id.loading_progressbar);
+        mWebView = findViewById(R.id.webview);
+
+        mWebView.loadUrl(urlToload);
+
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        //for progress  bar
+        mWebView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                mProgressBar.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                getSupportActionBar().setTitle(title);
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack())
+        {
+mWebView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+
     }
 }
